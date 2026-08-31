8de88b7aa93dfa1b53d92c4c3d1d8251eb83c7e7
muhammad02patel-jpg <muhammad.02patel@gmail.com>
2026-08-31 14:28:16 -0400

    Update drawstuff.js

--- drawstuff.js
+++ drawstuff.js
@@ -517,8 +517,12 @@ function main() {
     var testEye = new Vector(0,0,0);
     var testAt = Vector.subtract(new Vector(0,0,10),testEye);
     var view = {eye:testEye, at:testAt, up:new Vector(0,1,0)};
-    var poly = [{x:-5,y:5,z:10,c:new Color(255,0,0,255)}, {x:5,y:5,z:10,c:new Color(0,255,0,255)}, 
-                {x:5,y:-5,z:10,c:new Color(0,0,0,255)}, {x:-5,y:-5,z:10,c:new Color(0,0,255,255)}];
+    var poly = [
+    {x:-5, y:5,  z:10, c:new Color(255,0,0,255)},   // red point
+    {x:5,  y:5,  z:10, c:new Color(0,255,0,255)},   // green
+    {x:5,  y:-5, z:10, c:new Color(0,0,0,255)},     // black
+    {x:-2, y:-2, z:10, c:new Color(0,0,255,255)}    // blue
+];
     
     // Define and render a rectangle in 2D with colors and coords at corners
     projectPoly(imagedata,poly,view);
